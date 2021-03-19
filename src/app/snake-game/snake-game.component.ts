4e145b8832da14ec3fb511d59bea813be5756117
davedohc <davedohc@aim.com>
2021-03-19 21:40:00 +0000

    game html page and styling

--- src/app/snake-game/snake-game.component.ts
+++ src/app/snake-game/snake-game.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./snake-game.component.css']
 })
 export class SnakeGameComponent implements OnInit {
+points: number;
+  snakeBlocks: number;
+  speed: number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  playGame() {
+
+  }
+
+  pauseGame() {
+
+  }
+
+  resetGame() {
+
+  }
 }
