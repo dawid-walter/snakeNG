ea69aa036e2709baeb31ad6605bf8046c92660dc
davedohc <davedohc@aim.com>
2021-03-20 08:08:56 +0000

    basic snake animation and keyboard control

---
+++ src/app/snake-game/objects/key-code.ts
@@ -0,0 +1,6 @@
+export enum KEY_CODE {
+  KEY_UP = 'ArrowUp',
+  KEY_DOWN = 'ArrowDown',
+  KEY_RIGHT = 'ArrowRight',
+  KEY_LEFT = 'ArrowLeft',
+}
--- src/app/snake-game/snake-game.component.ts
+++ src/app/snake-game/snake-game.component.ts
@@ -1,6 +1,8 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import {Snake} from './objects/snake';
 import {BLOCK_SIZE, COLS, ROWS, SNAKE_COLOR} from './settings/global';
+import {DIRECTIONS} from './objects/directions';
+import {KEY_CODE} from './objects/key-code';
 
 @Component({
   selector: 'app-snake-game',
@@ -16,6 +18,7 @@ export class SnakeGameComponent implements OnInit {
   speed: number;
   time = {start: 0, elapsed: 0, total: 2000};
   snake: Snake;
+  movingDirection: DIRECTIONS;
 
 
   constructor() {
@@ -26,6 +29,22 @@ export class SnakeGameComponent implements OnInit {
     this.ctx.canvas.width = COLS * BLOCK_SIZE;
     this.ctx.canvas.height = ROWS * BLOCK_SIZE;
     this.snake = new Snake(SNAKE_COLOR);
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent): void {
+    if (event.code === KEY_CODE.KEY_DOWN) {
+      this.movingDirection = DIRECTIONS.DOWN;
+    }
+    if (event.code === KEY_CODE.KEY_UP) {
+      this.movingDirection = DIRECTIONS.UP;
+    }
+    if (event.code === KEY_CODE.KEY_LEFT) {
+      this.movingDirection = DIRECTIONS.LEFT;
+    }
+    if (event.code === KEY_CODE.KEY_RIGHT) {
+      this.movingDirection = DIRECTIONS.RIGHT;
+    }
   }
 
   playGame(): void {
@@ -44,12 +63,27 @@ export class SnakeGameComponent implements OnInit {
   private animate(now): void {
     this.time.elapsed = now - this.time.start;
     if (this.time.elapsed > 500) {
-      this.draw();
+      this.drawFrame();
       this.time.start = now;
     }
   }
 
-  draw(): void {
+  drawFrame(): void {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.updateStats();
+    this.drawSnake();
+    this.snake.moveSnakeTo(this.movingDirection);
+  }
 
+  drawSnake(): void {
+    this.ctx.fillStyle = this.snake.color;
+    this.snake.getSnake().forEach(snakeBlock => {
+      this.ctx.fillRect(snakeBlock.x, snakeBlock.y, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+    });
+  }
+
+  private updateStats(): void {
+    this.points++;
+    this.snakeBlocks = this.snake.snakeBody.length;
   }
 }
